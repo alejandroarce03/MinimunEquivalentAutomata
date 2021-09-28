ce1b88eebf5ad654bf1890f4d3967f4361f5c0ea
Alejandro <alejandrocutu1@gmail.com>
2021-09-28 12:10:43 -0500

    Generate-Mealy-Or-Moore

--- src/Automata.java
+++ src/Automata.java
@@ -4,11 +4,57 @@ public class Automata {
 
 	private ArrayList<State> states;
 	private ArrayList<Relation> relations;
-	
-	public Automata() {
-		
+
+	public Automata(String type,String pstates, String prelations) {
+		typeAutomata(type, pstates,prelations);
 	}
-	
-	
-	
+
+	public void typeAutomata(String type,String pstates, String prelations) {
+		if(type.equalsIgnoreCase("Mealy")) {
+			generateMealyAutomata(pstates,prelations);
+		}else {
+			generateMooreAutomata(pstates,prelations);
+		}
+	}
+
+	public void generateMealyAutomata(String pstates, String prelations) {
+
+		String[] statesStrings = pstates.split(";");
+
+		for(int i=0;i>statesStrings.length;i++) {
+			State s = new State(statesStrings[i],null);
+			states.add(s);
+		}
+
+		String[] relationsStrings = prelations.split(";");
+
+		for(int i=0;i>relationsStrings.length;i++) {
+			String[] relationsInOut = relationsStrings[i].split(",");
+			Relation r = new Relation(relationsInOut[0],relationsInOut[1],relationsInOut[2],relationsInOut[3]);
+			relations.add(r);
+		}
+
+	}
+
+	public void generateMooreAutomata(String pstates, String prelations) {
+
+		String[] statesStrings = pstates.split(";");
+
+		for(int i=0;i>statesStrings.length;i++) {
+			String[] statesNameValue = statesStrings[i].split(",");
+			State s = new State(statesNameValue[0],statesNameValue[1]);
+			states.add(s);
+		}
+
+		String[] relationsStrings = prelations.split(";");
+
+		for(int i=0;i>relationsStrings.length;i++) {
+			String[] relationsInOut = relationsStrings[i].split(",");
+			Relation r = new Relation(relationsInOut[0],null,relationsInOut[2],relationsInOut[3]);
+			relations.add(r);
+		}
+
+	}
+
+
 }
--- src/Relation.java
+++ src/Relation.java
@@ -3,9 +3,13 @@ public class Relation {
 	
 	private String input;
 	private String output;
+	private String sourceState;
+	private String destinationState;
 	
-	public Relation(String i,String o) {
+	public Relation(String i,String o,String srcState,String dstState) {
 		input = i;
 		output = o;
+		sourceState = srcState;
+		destinationState = dstState;
 	}
 }
